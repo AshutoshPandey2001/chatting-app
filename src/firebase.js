0ae8f650eacf88ec1da79172196a9e4b18a7a330
Ashutosh Pandey <ap1495813@gmail.com>
2023-06-11 18:08:42 +0530

    third commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCuaSR4615QdheoEHK5SDZQX9k1R91vFd0",
     authDomain: "chat-app-react-5d4b4.firebaseapp.com",
@@ -12,3 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore(app);
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style.scss'
 import Add from '../img/addAvatar.png'
+import { auth, db, storage } from '../firebase'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
+    const [err, setErr] = useState(false)
+
+
+    const handleSumbit = async (e) => {
+        e.preventDefault()
+        const displayName = e.target[0].value
+        const email = e.target[1].value
+        const password = e.target[2].value
+        const file = e.target[3].files[0];
+
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password)
+            const date = new Date().getTime();
+            const storageRef = ref(storage, `${displayName + date}`);
+
+            await uploadBytesResumable(storageRef, file).then(() => {
+                getDownloadURL(storageRef).then(async (downloadURL) => {
+                    try {
+                        //Update profile
+                        await updateProfile(res.user, {
+                            displayName,
+                            photoURL: downloadURL,
+                        });
+                        //create user on firestore
+                        await setDoc(doc(db, "users", res.user.uid), {
+                            uid: res.user.uid,
+                            displayName,
+                            email,
+                            photoURL: downloadURL,
+                        });
+
+                        //create empty user chats on firestore
+                        await setDoc(doc(db, "userChats", res.user.uid), {});
+                    } catch (err) {
+                        console.log(err);
+                        setErr(true);
+                    }
+                });
+            });
+        } catch (error) {
+            console.log('error', error);
+            setErr(true)
+        }
+        console.log('values', displayName, email, password);
+
+
+    }
+
     return (
         <div className='formContainer'>
             <div className='fromWrapper'>
                 <span className='logo'>Chatting App</span>
                 <span className='title'>Register</span>
-                <form>
+                <form onSubmit={handleSumbit}>
                     <input type='text' placeholder='displayname' />
                     <input type='email' placeholder='email' />
                     <input type='password' placeholder='password' />
@@ -18,6 +70,7 @@ const Register = () => {
                         <span>Add an avatar</span>
                     </label>
                     <button>Sign Up</button>
+                    {err && <span>Something Went Worng</span>}
                 </form>
                 <p>You do have an account ? Login</p>
             </div>
